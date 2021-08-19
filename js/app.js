b7d98c6fe53fb5c3478b20cad64939b3cb7bc54c
Srdjan Lukic <srdjan@lukic.nu>
2021-08-19 08:30:40 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/*
+Text: 
+   It was 94 fahrenheit outside, so :insertName: went for a walk. When they got
+      to :insertLocation:, they stared in horror for a few moments, then :insertEvent:. Bob
+      saw the whole thing, but was not surprised — :insertName: weighs 300 pounds,
+      and it was a hot day.
+
+Name: 
+Willy the Goblin
+Big Daddy
+Father Christmas
+
+
+Location:
+the soup kitchen
+Disneyland
+the White House
+
+Event:
+spontaneously combusted
+melted into a puddle on the sidewalk
+turned into a slug and crawled away
+*/
